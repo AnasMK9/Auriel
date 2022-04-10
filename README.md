7096a5fe638a24fdf98a1701cf890e09c42cffdd
Anas <a.khudeirat@hotmail.com>
2022-04-10 23:06:07 +0300

    fixed last commit

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Client that listens to commands on slack channel and executes on local machine, 
 Originally intended to running multiple instances of each tool on a virtual machine, ended up liking the idea of containerizing the tools and spawning them using a k8s engine more. each tool will recieve commands through a specific channel, and will have a class configured to handle its options and spawn a container through gcp or some other cloud provider. A Router class will be responsible for to mapping the channels with the actual classes. idk if it's necessary yet but thought it's fancier and will improve scalability in the future to add as many tools and channels as needed.
 
 # Running
-current main branch is a big mess, last commit that actually executes is f38dd1be9b7fa39bde86733ceeba074bddc2b0d8
+~~current main branch is a big mess, last commit that actually executes is f38dd1be9b7fa39bde86733ceeba074bddc2b0d8~~
 1- install requirements using 
 ```
 pip install -r requirements.txt
--- auriel/app.py
+++ auriel/app.py
@@ -22,7 +22,7 @@ def router(event, say):
 
 
 if __name__ == '__main__':
-    # app.start(port=Config.PORT)
+    app.start(port=Config.PORT)
 
     # message = {'token': 'mirwmsEjdkEoaaXfTbatU0qd', 'team_id': 'T03B7U1JK7B', 'api_app_id': 'A03AEPHQ1GF', 'event': {'client_msg_id': '2df7c978-658c-4b58-8a70-2fa2c9a49f17', 'type': 'app_mention', 'text': '<@U03AEPJ8LP9> -org:shein -cloneForks', 'user': 'U03AVA83V0T', 'ts': '1649542287.794229', 'team': 'T03B7U1JK7B', 'blocks': [{'type': 'rich_text', 'block_id': '2CR3', 'elements': [{'type': 'rich_text_section', 'elements': [{'type': 'user', 'user_id': 'U03AEPJ8LP9'}, {'type': 'text', 'text': ' -org:shein -cloneForks'}]}]}], 'channel': 'C03AVAK00LA', 'event_ts': '1649542287.794229'}, 'type': 'event_callback', 'event_id': 'Ev03ASJ6NQ13', 'event_time': 1649542287, 'authorizations': [{'enterprise_id': None, 'team_id': 'T03B7U1JK7B', 'user_id': 'U03AEPJ8LP9', 'is_bot': True, 'is_enterprise_install': False}], 'is_ext_shared_channel': False, 'event_context': '4-eyJldCI6ImFwcF9tZW50aW9uIiwidGlkIjoiVDAzQjdVMUpLN0IiLCJhaWQiOiJBMDNBRVBIUTFHRiIsImNpZCI6IkMwM0FWQUswMExBIn0'}
     # event = {
@@ -51,4 +51,3 @@ if __name__ == '__main__':
     #     "event_ts": "1649542590.206229",
     # }
     # router(event)
-    # recieve_command(event, print)
